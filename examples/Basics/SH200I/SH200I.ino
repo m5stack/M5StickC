76a2380e5d98ea785fa5f9d73998e99bfcae9ebb
yalezy <zhouyangyale@gmail.com>
2019-03-25 10:24:31 +0800

    upadte

--- examples/Basics/SH200I/SH200I.ino
+++ examples/Basics/SH200I/SH200I.ino
@@ -1,19 +1,5 @@
 #include <M5StickC.h>
-#include <IMU.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  M5.begin();
-  sh200i_init();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.println("SH200I TEST");
-  M5.Lcd.println();
-  M5.Lcd.println("    X       Y       Z");
-}
-
-long loopTime, startTime, endTime, fps;
 int16_t accX = 0;
 int16_t accY = 0;
 int16_t accZ = 0;
@@ -22,46 +8,39 @@ int16_t gyroX = 0;
 int16_t gyroY = 0;
 int16_t gyroZ = 0;
 
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  M5.IMU.Init();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setCursor(40, 0);
+  M5.Lcd.println("SH200I TEST");
+  M5.Lcd.setCursor(0, 15);
+  M5.Lcd.println("  X       Y       Z");
+}
+
+
+
+int16_t temp = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned char tempdata[1];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X, 1, tempdata);
-  accX = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+1, 1, tempdata);
-  accX |= tempdata[0] << 8;
-
-
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+2, 1, tempdata);
-  accY = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+3, 1, tempdata);
-  accY |= tempdata[0] << 8;
-    
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+4, 1, tempdata);
-  accZ = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+5, 1, tempdata);
-  accZ |= tempdata[0] << 8;
-
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+6, 1, tempdata);
-  gyroX = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+7, 1, tempdata);
-  gyroX |= tempdata[0] << 8;
-
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+8, 1, tempdata);
-  gyroY = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+9, 1, tempdata);
-  gyroY |= tempdata[0] << 8;
-
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+10, 1, tempdata);
-  gyroZ = tempdata[0];
-  I2C_Read_NBytes(SH200I_ADDRESS, SH200I_OUTPUT_ACC_X+11, 1, tempdata);
-  gyroZ |= tempdata[0] << 8;
-
-
-  Serial.printf("aX = %ld,aY = %ld,aZ = %ld,gX = %ld,gY = %ld,gZ = %ld\r\n", accX, accY, accZ,gyroX, gyroY, gyroZ);
-  M5.Lcd.setCursor(0, 30, 1);
-  M5.Lcd.printf("  %ld    %ld    %ld\r\n", accX, accY, accZ);
-  M5.Lcd.println();
-  M5.Lcd.printf("  %ld      %ld     %ld   ", gyroX, gyroY, gyroZ);
+  M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+  M5.IMU.getAccelData(&accX,&accY,&accZ);
+  M5.IMU.getTempData(&temp);
+  
+  M5.Lcd.setCursor(0, 30);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ", ((float) gyroX) * M5.IMU.gRes, ((float) gyroY) * M5.IMU.gRes,((float) gyroZ) * M5.IMU.gRes);
+  M5.Lcd.setCursor(140, 30);
+  M5.Lcd.print("mg");
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ",((float) accX) * M5.IMU.aRes,((float) accY) * M5.IMU.aRes, ((float) accZ) * M5.IMU.aRes);
+  M5.Lcd.setCursor(140, 45);
+  M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.printf("Temperature : %.2f C",((float) temp) / 333.87 + 21.0);
   delay(1000);
 
 }
