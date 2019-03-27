1ea5121d9c59e88ef189308f006dbfc611f8ab66
yalezy <zhouyangyale@gmail.com>
2019-03-27 15:18:27 +0800

    updata display

--- src/M5Display.cpp
+++ src/M5Display.cpp
@@ -1,6 +1,10 @@
 #include "M5Display.h"
 
-M5Display::M5Display() : TFT_eSPI() {}
+M5Display::M5Display() : TFT_eSPI() {
+
+  hzk16Used = false;
+  hzk16Type = DontUsedHzk16;
+}
 
 void M5Display::begin() {
    TFT_eSPI::begin();
@@ -31,6 +35,408 @@ void M5Display::drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, uint8_t
   setSwapBytes(true);
   pushImage((int32_t)x0, (int32_t)y0, (uint32_t)w, (uint32_t)h, (uint16_t*)data);
 }
+
+
+
+/***************************************************************************************
+
+** Function name:           loadHzk16
+
+** Description:             loadHzk16 fonts
+
+** Params:
+
+**  HZK16Path: HZK16 file path on TF card. e.g. /HZK16, means file on the
+
+*root dir of TF card. * ASC16Path: ASC16 file path on TF card.
+
+***************************************************************************************/
+
+void M5Display::loadHzk16(Hzk16Types hzkTypes,const char *HZK16Path, const char *ASC16Path) {
+
+  if (hzk16Used)
+
+    return;
+
+
+//Hzk16Types hzkTypes = InternalHzk16
+if(hzkTypes == InternalHzk16){
+//#if defined(_ASC16_) && defined(_HZK16_)
+
+  pAscCharMatrix = (uint8_t *)&ASC16[0];
+
+  pGbkCharMatrix = (uint8_t *)&HZK16[0];
+
+  Serial.println("HZK16 path: Internal");
+
+  Serial.println("ASC16 path: Internal");
+
+  hzk16Used = initHzk16(true, nullptr, nullptr);
+}
+
+  Serial.print("HZK16 init result: ");
+
+  Serial.println(isHzk16Used());
+
+}
+
+
+bool M5Display::initHzk16(boolean use, const char *HZK16Path,
+                        const char *ASC16Path) {
+
+  bool result = false;
+
+  if (use == false) { // Do not use HZK16 and ASC16 fonts
+
+    hzk16Type = DontUsedHzk16;
+
+    Serial.println("Use default font.");
+
+  } else if (pAscCharMatrix == NULL ||
+
+             pGbkCharMatrix ==
+
+                 NULL) { // Use external HZK16 and ASC16 font on TF card.
+
+      hzk16Type = DontUsedHzk16;
+
+      Serial.println("External font file HZK16/ASC16 lost, use default font.");
+
+  } else { // Use internal HZK16 and ASC16 fonts
+
+    hzk16Type = InternalHzk16;
+
+    Serial.println("Use internal HZK16 and ASC16 font.");
+
+  }
+
+
+
+  switch (hzk16Type) {
+
+
+
+  case InternalHzk16: {
+
+    if (pAscCharMatrix == NULL || pGbkCharMatrix == NULL) {
+
+      hzk16Type = DontUsedHzk16;
+
+    }
+
+
+
+    if (pHzk16File != NULL) {
+
+      pHzk16File->close();
+
+      pHzk16File = NULL;
+
+    }
+
+    if (pAsc16File != NULL) {
+
+      pAsc16File->close();
+
+      pAsc16File = NULL;
+
+    }
+
+    hzkBufCount = 0;
+
+    break;
+
+  }
+
+  case DontUsedHzk16: {
+
+    if (pHzk16File != NULL) {
+
+      pHzk16File->close();
+
+      pHzk16File = NULL;
+
+    }
+
+    if (pAsc16File != NULL) {
+
+      pAsc16File->close();
+
+      pAsc16File = NULL;
+
+    }
+
+    break;
+
+  }
+
+  }
+
+  return hzk16Type != DontUsedHzk16;
+
+}
+
+void M5Display::writeHzk(char* c){
+
+  char *ret  = c;
+
+  ascCharWidth = 8 * textsize;
+
+  ascCharHeigth = 16 * textsize;
+
+  gbkCharWidth = ascCharHeigth;
+
+  gbkCharHeight = gbkCharWidth;
+  if(ret == NULL) return;
+  while(*ret != '\0'){
+    while(*ret <= 0xA0){
+      if(*ret == '\0') return;
+      writeHzkAsc(*ret);
+      ret++;  
+    }
+    writeHzkGbk(ret);
+    ret++;
+    ret++;
+  }
+}
+
+void M5Display::writeHzkAsc(const char c) {
+
+  if (c == '\n') {
+
+    cursor_x = 0;
+
+    cursor_y += ascCharHeigth;
+
+  } else if (c != '\r') {
+
+    uint32_t offset;
+
+    uint8_t mask;
+
+    uint16_t posX = cursor_x, posY = cursor_y;
+
+    uint8_t charMatrix[16];
+
+    uint8_t *pCharMatrix;
+
+
+
+    offset = (uint32_t)c * 16;
+
+
+
+    if (hzk16Type == ExternalHzk16) {
+
+      pAsc16File->seek(offset, SeekSet);
+
+      pAsc16File->readBytes((char *)&charMatrix[0], 16);
+
+      pCharMatrix = &charMatrix[0];
+
+    } else {
+
+      if (pAscCharMatrix == NULL) {
+
+        return;
+
+      }
+
+      pCharMatrix = pAscCharMatrix + offset;
+
+    }
+
+
+
+    // startWrite();
+
+
+
+    if (highlighted) {
+
+      fillRect(cursor_x, cursor_y, ascCharWidth, ascCharHeigth, highlightcolor);
+
+    } else if (istransparent == false) {
+
+      fillRect(cursor_x, cursor_y, ascCharWidth, ascCharHeigth, textbgcolor);
+
+    }
+
+
+
+    for (uint8_t row = 0; row < 16; row++) {
+
+      mask = 0x80;
+
+      posX = cursor_x;
+
+      for (uint8_t col = 0; col < 8; col++) {
+
+        if ((*pCharMatrix & mask) != 0) {
+
+          if (textsize == 1) {
+
+            drawPixel(posX, posY, textcolor);
+
+          } else {
+
+            fillRect(posX, posY, textsize, textsize, textcolor);
+
+          }
+
+        }
+
+        posX += textsize;
+
+        mask >>= 1;
+
+      }
+
+      posY += textsize;
+
+      pCharMatrix++;
+
+    }
+
+
+
+    // endWrite();
+
+
+
+    cursor_x += ascCharWidth;
+
+    if (textwrap && ((cursor_x + ascCharWidth) > _width)) {
+
+      cursor_x = 0;
+
+      cursor_y += ascCharHeigth;
+
+    }
+
+  }
+
+}
+
+
+void M5Display::writeHzkGbk(const char *c) {
+
+  uint32_t offset;
+
+  uint8_t mask;
+
+  uint16_t posX = cursor_x;
+  uint16_t posY = cursor_y;
+
+  uint8_t charMatrix[32];
+
+  uint8_t *pCharMatrix;
+
+
+
+  offset = (uint32_t)(94 * (uint32_t)(c[0] - 0xA1) + (uint32_t)(c[1] - 0xA1)) * 32;
+
+  if (hzk16Type == ExternalHzk16) {
+
+    pHzk16File->seek(offset, SeekSet);
+
+    pHzk16File->readBytes((char *)&charMatrix[0], 32);
+
+    pCharMatrix = &charMatrix[0];
+
+  } else {
+
+    if (pGbkCharMatrix == NULL) {
+
+      return;
+
+    }
+
+    pCharMatrix = pGbkCharMatrix + offset;
+
+  }
+
+
+
+  // startWrite();
+
+
+
+  if (highlighted) {
+
+    fillRect(cursor_x, cursor_y, gbkCharWidth, gbkCharHeight, highlightcolor);
+
+  } else if (istransparent == false) {
+
+    fillRect(cursor_x, cursor_y, gbkCharWidth, gbkCharHeight, textbgcolor);
+
+  }
+
+
+
+  for (uint8_t row = 0; row < 16; row++) {
+
+    posX = cursor_x;
+
+    mask = 0x80;
+
+    for (uint8_t col = 0; col < 8; col++) {
+
+      if ((*pCharMatrix & mask) != 0) {
+
+        if (textsize == 1) {
+
+          drawPixel(posX, posY, textcolor);
+
+        } else {
+
+          fillRect(posX, posY, textsize, textsize, textcolor);
+
+        }
+
+      }
+
+      if ((*(pCharMatrix + 1) & mask) != 0) {
+
+        if (textsize == 1) {
+
+          drawPixel(posX + ascCharWidth, posY, textcolor);
+
+        } else {
+
+          fillRect(posX + ascCharWidth, posY, textsize, textsize, textcolor);
+
+        }
+
+      }
+
+      mask >>= 1;
+
+      posX += textsize;
+
+    }
+
+    posY += textsize;
+
+    pCharMatrix += 2;
+
+  }
+
+  // endWrite();
+
+  cursor_x += gbkCharWidth;
+
+  if (textwrap && ((cursor_x + gbkCharWidth) > _width)) {
+
+    cursor_x = 0;
+
+    cursor_y += gbkCharHeight;
+
+  }
+
+}
+
+void M5Display::setTextWrap(boolean w) { textwrap = w; }
 #if 0
 void M5Display::sleep() {
   startWrite();
--- src/M5Display.h
+++ src/M5Display.h
@@ -6,6 +6,8 @@
 #include <SPI.h>
 #include "utility/In_eSPI.h"
 #include "utility/Sprite.h"
+#include "Fonts/HZK16.h"
+#include "Fonts/ASC16.h"
 
 typedef enum {
   JPEG_DIV_NONE,
@@ -14,6 +16,14 @@ typedef enum {
   JPEG_DIV_8,
   JPEG_DIV_MAX
 } jpeg_div_t;
+
+
+typedef enum
+{
+  DontUsedHzk16,
+  InternalHzk16,
+  ExternalHzk16
+}Hzk16Types;
 
 class M5Display : public TFT_eSPI {
 
@@ -26,7 +36,18 @@ class M5Display : public TFT_eSPI {
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, const uint8_t *data);
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, uint16_t *data);
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, uint8_t *data);
-  void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, const uint16_t *data, uint16_t transparent);/*
+  void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, const uint16_t *data, uint16_t transparent);
+  
+  
+  void loadHzk16(Hzk16Types hzkTypes = InternalHzk16,const char* HZK16Path = "/HZK16", const char* ASC16Path = "/ASC16");
+  void setTextWrap(boolean wrap);
+
+  void writeHzk(char* c);
+  // Highlight the text (Once set to be true, the text background will not be transparent any more)
+  inline void highlight(bool isHighlight) { highlighted = isHighlight; }
+  // Set highlight color
+  inline void setHighlightColor(uint16_t color) { highlightcolor = color; istransparent = false; }
+  /*
   void clearDisplay(uint32_t color=ILI9341_BLACK) { fillScreen(color); }
   void clear(uint32_t color=ILI9341_BLACK) { fillScreen(color); }
   void display() {}
@@ -79,5 +100,35 @@ class M5Display : public TFT_eSPI {
                    jpeg_div_t scale = JPEG_DIV_NONE);*/
 
  private:
+
+ void writeHzkAsc(const char c);
+ void writeHzkGbk(const char *c);
+
+ bool initHzk16(boolean use, const char *HZK16Path,
+                        const char *ASC16Path);
+
+ inline bool isHzk16Used(){return hzk16Used;}
+
+ boolean hzk16Used,istransparent,
+          highlighted;
+
+ uint8_t hzkBufCount;
+
+ uint8_t *pAscCharMatrix, *pGbkCharMatrix; 
+
+  uint16_t
+          highlightcolor,
+          ascCharWidth, 
+          ascCharHeigth, 
+          gbkCharWidth, 
+          gbkCharHeight;
+
+ Hzk16Types  hzk16Type;          // Use of HZK16 and ASC16 font. 
+
+ File
+    Asc16File, Hzk16File,   // Font file
+    *pAsc16File, *pHzk16File; // Font file pointer
+  
+  boolean  textwrap; // If set, 'wrap' text at right edge of display
 };
 #endif
