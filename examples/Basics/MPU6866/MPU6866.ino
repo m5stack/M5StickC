1a759d97a4a8e1ea98031f551784ec6b83c9b920
yalezy <zhouyangyale@gmail.com>
2019-06-17 11:10:03 +0800

    add 6866 example

---
+++ examples/Basics/MPU6866/MPU6866.ino
@@ -0,0 +1,42 @@
+#include <M5StickC.h>
+
+float accX = 0;
+float accY = 0;
+float accZ = 0;
+
+float gyroX = 0;
+float gyroY = 0;
+float gyroZ = 0;
+
+float temp = 0;
+void setup() {
+  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setCursor(40, 0);
+  M5.Lcd.println("MPU6866 TEST");
+  M5.Lcd.setCursor(0, 15);
+  M5.Lcd.println("  X       Y       Z");
+  M5.MPU6866.Init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  M5.MPU6866.getGyroData(&gyroX,&gyroY,&gyroZ);
+  M5.MPU6866.getAccelData(&accX,&accY,&accZ);
+  M5.MPU6866.getTempData(&temp);
+
+  M5.Lcd.setCursor(0, 30);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ", gyroX, gyroY,gyroZ);
+  M5.Lcd.setCursor(140, 30);
+  M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf("%.2f   %.2f   %.2f      ",accX * 1000,accY * 1000, accZ * 1000);
+  M5.Lcd.setCursor(140, 45);
+  M5.Lcd.print("mg");
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.printf("Temperature : %.2f C", temp);
+  delay(100);
+}
