67850f315d912df2b32c0ce301bed3e8fe88407b
Nakano Yosuke <ynakano@moff.mobi>
2019-07-25 00:19:58 +0900

    fix reading values and format.

--- examples/Basics/SH200I/SH200I.ino
+++ examples/Basics/SH200I/SH200I.ino
@@ -1,12 +1,12 @@
 #include <M5StickC.h>
 
-int16_t accX = 0;
-int16_t accY = 0;
-int16_t accZ = 0;
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
 
-int16_t gyroX = 0;
-int16_t gyroY = 0;
-int16_t gyroZ = 0;
+float gyroX = 0.0F;
+float gyroY = 0.0F;
+float gyroZ = 0.0F;
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,9 +21,7 @@ void setup() {
   M5.Lcd.println("  X       Y       Z");
 }
 
-
-
-int16_t temp = 0;
+float temp = 0;
 /*****************************************
 M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
 M5.IMU.getAccelData(&accX,&accY,&accZ);
@@ -31,20 +29,19 @@ M5.IMU.getTempData(&temp);
 *****************************************/
 void loop() {
   // put your main code here, to run repeatedly:
-  M5.IMU.getGyroAdc(&gyroX,&gyroY,&gyroZ);
-  M5.IMU.getAccelAdc(&accX,&accY,&accZ);
-  M5.IMU.getTempAdc(&temp);
+  M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+  M5.IMU.getAccelData(&accX,&accY,&accZ);
+  M5.IMU.getTempData(&temp);
   
   M5.Lcd.setCursor(0, 30);
-  M5.Lcd.printf("%.2f   %.2f   %.2f      ", ((float) gyroX) * M5.IMU.gRes, ((float) gyroY) * M5.IMU.gRes,((float) gyroZ) * M5.IMU.gRes);
+  M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", gyroX, gyroY, gyroZ);
   M5.Lcd.setCursor(140, 30);
-  M5.Lcd.print("mg");
-  M5.Lcd.setCursor(0, 45);
-  M5.Lcd.printf("%.2f   %.2f   %.2f      ",((float) accX) * M5.IMU.aRes,((float) accY) * M5.IMU.aRes, ((float) accZ) * M5.IMU.aRes);
-  M5.Lcd.setCursor(140, 45);
   M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", accX, accY, accZ);
+  M5.Lcd.setCursor(140, 45);
+  M5.Lcd.print("G");
   M5.Lcd.setCursor(0, 60);
-  M5.Lcd.printf("Temperature : %.2f C",((float) temp) / 333.87 + 21.0);
+  M5.Lcd.printf("Temperature : %.2f C", temp);
   delay(1000);
-
 }
