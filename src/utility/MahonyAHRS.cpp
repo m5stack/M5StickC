f665fd3696fe9a82cfeb0e0c028b461b90032360
tanakamasayuki <tanakamasayuki@gmail.com>
2019-08-21 17:39:15 +0900

    Fixed wrong result of invSqrt

--- src/utility/MahonyAHRS.cpp
+++ src/utility/MahonyAHRS.cpp
@@ -235,17 +235,10 @@ void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float
 }
 
 //---------------------------------------------------------------------------------------------------
-// Fast inverse square-root
-// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+// square-root
 
 float invSqrt(float x) {
-	float halfx = 0.5f * x;
-	float y = x;
-	long i = *(long*)&y;
-	i = 0x5f3759df - (i>>1);
-	y = *(float*)&i;
-	y = y * (1.5f - (halfx * y * y));
-	return y;
+	return sqrt(x);
 }
 
 //====================================================================================================
