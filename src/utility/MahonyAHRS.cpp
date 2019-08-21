bf23c07f61bb8c276b124c89e2af516aac774fcd
tanakamasayuki <tanakamasayuki@gmail.com>
2019-08-21 19:34:17 +0900

    Fix compile warning(invSqrt)

--- src/utility/MahonyAHRS.cpp
+++ src/utility/MahonyAHRS.cpp
@@ -239,11 +239,16 @@ void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float
 // See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
 
 float invSqrt(float x) {
+	typedef union {
+		float f;
+		long l;
+	} union_t;
+
 	float halfx = 0.5f * x;
 	float y = x;
-	long i = *(long*)&y;
+	long i = ((union_t*)&y)->l;
 	i = 0x5f3759df - (i>>1);
-	y = *(float*)&i;
+	y = ((union_t*)&i)->f;
 	y = y * (1.5f - (halfx * y * y));
 	return y;
 }
