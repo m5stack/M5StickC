fdfd245ab4e95fdfc58a16ab89508c5b2151a63f
Haifeng <1981297702@qq.com>
2019-09-27 17:02:05 +0800

    Add an example hat for SERVOS

--- examples/Hat/SERVOS/IIC_servo.h
+++ examples/Hat/SERVOS/IIC_servo.h
@@ -5,13 +5,13 @@
 
 #define SERVO_ADDRESS	0X38
 
-
 void IIC_Servo_Init();	//sda  0     scl  26		
 
-uint8_t Servo_angle_set(uint8_t Servo_CH,uint8_t angle);
-
+//Servo_CH: 1-8 angle:  0-180
+uint8_t Servo_angle_set(uint8_t Servo_CH,uint8_t angle);         
+//Servo_CH: 1-8 width(us):  500-2500    
 uint8_t Servo_pulse_set(uint8_t Servo_CH,uint16_t width);
-
+//R: 0-255  G:  0-255   B:  0-255
 uint8_t RGB_set(uint8_t R,uint8_t G,uint8_t B);
 
 uint8_t Servo_angle_read(uint8_t Servo_CH);
@@ -19,7 +19,6 @@ uint8_t Servo_angle_read(uint8_t Servo_CH);
 uint16_t Servo_pulse_read(uint8_t Servo_CH);
 
 uint32_t RGB_read();
-
 
 
 #endif
--- examples/Hat/SERVOS/SERVOS.ino
+++ examples/Hat/SERVOS/SERVOS.ino
@@ -1,59 +1,59 @@
 #include <M5StickC.h>
 #include "IIC_servo.h"
 
-
-
-
 // the setup routine runs once when M5StickC starts up
 void setup(){ 
   
-  // Initialize the M5StickC object
-  M5.begin();
-
-  // LCD display
-  M5.Lcd.println(" ");
-  M5.Lcd.println("  IIC_servo");
-
-  IIC_Servo_Init();     //sda  0     scl  26 
-
-//  Servo_angle_set(1,90);     // CH1     ，0°
-//  Servo_angle_set(2,180);   // CH2     ，180°
-//  
-//  Servo_pulse_set(3,2500);  //CH3      ，2500us High level ==  180°
-//  Servo_pulse_set(4,1500);  //CH4      ，1500us High level ==  90°
-  
-  
-
-  M5.Lcd.println("  angle :");
-  M5.Lcd.print("    ");
-  M5.Lcd.println(Servo_angle_read(3));    //CH3
-  delay(1000);
-  M5.Lcd.println("  pulse :");
-  M5.Lcd.print("    ");
-  M5.Lcd.println(Servo_pulse_read(3));    //CH3
-  delay(1000);
-  M5.Lcd.println("  RGB :");
-  M5.Lcd.print("    ");
-  M5.Lcd.println(RGB_read());             //RGB
+    // Initialize the M5StickC object
+    M5.begin();
+    // LCD display
+    M5.Lcd.println(" ");
+    M5.Lcd.println("  IIC_servo");
+    IIC_Servo_Init();     //sda  0     scl  26 
+    //  Servo_angle_set(1,90);      // CH1     , 0°
+    //  Servo_angle_set(2,180);     // CH2     ，180°
+    //  Servo_pulse_set(3,2500);    //CH3      ，2500us High level ==  180°
+    //  Servo_pulse_set(4,1500);    //CH4      ，1500us High level ==  90°
+    M5.Lcd.println("  angle :");
+    M5.Lcd.print("    ");
+    M5.Lcd.println(Servo_angle_read(3));    //CH3
+    delay(1000);
+    M5.Lcd.println("  pulse :");
+    M5.Lcd.print("    ");
+    M5.Lcd.println(Servo_pulse_read(3));    //CH3
+    delay(1000);
+    M5.Lcd.println("  RGB :");
+    M5.Lcd.print("    ");
+    M5.Lcd.println(RGB_read());             //RGB
 }
 
 // the loop routine runs over and over again forever
 void loop() {
-  RGB_set(1,0,0);      // red
-  delay(200);
-  RGB_set(0,1,0);      // green
-  delay(200);
-  RGB_set(0,0,1);      // blue
-  delay(200);
-  RGB_set(1,1,1);  //RGB   white
-  
-  for(int i=1;i<9;i++)
-  {
-    Servo_angle_set(i,0);     //
-  }
-  delay(1000); 
-  for(int i=1;i<9;i++)
-  {
-    Servo_angle_set(i,180);     //
-  }
+    RGB_set(1,0,0);      // red
+    delay(200);
+    RGB_set(0,1,0);      // green
+    delay(200);
+    RGB_set(0,0,1);      // blue
+    delay(200);
+    RGB_set(1,1,1);     //RGB   white
+
+    // for(int i=1;i<9;i++)
+    // {
+    //     Servo_angle_set(i,0);       //
+    // }
+    // delay(1000); 
+    // for(int i=1;i<9;i++)
+    // {
+    //     Servo_angle_set(i,180);     //
+    // }
+
+     for(int i=1;i<9;i++)
+    {
+        Servo_pulse_set(i,500);       //
+    }
+    delay(1000); 
+    for(int i=1;i<9;i++)
+    {
+        Servo_pulse_set(i,2500);     //
+    }
 }
