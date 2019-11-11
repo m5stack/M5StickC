bab14990159d794794599afca3fc85049f56388d
sakabin <heycoolbin@gmail.com>
2019-11-11 17:40:27 +0800

    Add unit max30100 example

--- examples/Unit/HEART/MAX30100_RawData/MAX30100_RawData.ino
+++ examples/Unit/HEART/MAX30100_RawData/MAX30100_RawData.ino
@@ -1,24 +1,8 @@
 /*
-Arduino-MAX30100 oximetry / heart rate integrated sensor library
-Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    pls install MAX30100lib by library manager first
+    addr: https://github.com/oxullo/Arduino-MAX30100
 */
 
-// The example shows how to retrieve raw values from the sensor
-// experimenting with the most relevant configuration parameters.
-// Use the "Serial Plotter" app from arduino IDE 1.6.7+ to plot the output
 #include <M5StickC.h>
 #include <Wire.h>
 #include "MAX30100.h"
