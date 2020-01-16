cf43023b1f9f413a22c12ee3de8134ef08ee392c
root <akiraneko@gmail.com>
2020-01-16 13:53:24 +0900

    Fix TFT_Clock (Screen size update)

--- examples/Advanced/Display/TFT_Clock/TFT_Clock.ino
+++ examples/Advanced/Display/TFT_Clock/TFT_Clock.ino
@@ -22,7 +22,7 @@
 
 float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
 float sdeg=0, mdeg=0, hdeg=0;
-uint16_t osx=120, osy=120, omx=120, omy=120, ohx=120, ohy=120;  // Saved H, M, S x & y coords
+uint16_t osx=40, osy=40, omx=40, omy=40, ohx=40, ohy=40;  // Saved H, M, S x & y coords
 uint16_t x0=0, x1=0, yy0=0, yy1=0;
 uint32_t targetTime = 0;                    // for next 1 second timeout
 
