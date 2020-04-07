a611452207786dc778fb45195d69dc954561aa6d
Florian <1technophile@users.noreply.github.com>
2020-04-07 16:57:46 -0500

    SetBrightness is not available for M5Stick C
    M5.Axp.ScreenBreath(X)
    7<=X<=15
    
    Should be used

--- src/M5Display.h
+++ src/M5Display.h
@@ -31,7 +31,6 @@ class M5Display : public TFT_eSPI {
   M5Display();
   void begin();
   void sleep();
-  void setBrightness(uint8_t brightness);
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, const uint16_t *data);
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, const uint8_t *data);
   void drawBitmap(int16_t x0, int16_t y0, int16_t w, int16_t h, uint16_t *data);
