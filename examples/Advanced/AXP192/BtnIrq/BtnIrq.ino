df5c99f4cea87e5354d2b63f22eb937ffd90a0f3
sorznal <heycoolbin@gmail.com>
2020-06-01 15:16:40 +0800

    Update axp192 irq example (reset imu irq)

--- examples/Advanced/AXP192/BtnIrq/BtnIrq.ino
+++ examples/Advanced/AXP192/BtnIrq/BtnIrq.ino
@@ -1,5 +1,19 @@
 #include "M5StickC.h"
 
+static void clear_mpu6880IRQ() {
+  uint8_t temp;
+  // set int active is LOW
+  M5.I2C.writeByte(0x68, 0x37, 0xc8);
+  // disable irq
+  M5.I2C.writeByte(0x68, 0x38, 0x00);
+  // clear irq status
+  M5.I2C.readByte(0x68, 0x39, &temp);
+}
+
+static void clear_200qIRQ() {
+  M5.I2C.writeByte(0x6C, 0x13, 0xe0);
+}
+
 static void axp192_disableAllIRQ() {
   M5.I2C.writeByte(0x34, 0x40, 0x00);
   M5.I2C.writeByte(0x34, 0x41, 0x00);
@@ -32,6 +46,8 @@ void setup() {
   M5.Lcd.setRotation(3);
 
   Wire.begin(21, 22, 400000);
+  clear_mpu6880IRQ();
+  clear_200qIRQ();
   pinMode(35, INPUT);
   axp192_disableAllIRQ();
   axp192_clearAllIRQStatus();
