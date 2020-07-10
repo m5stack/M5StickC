3696c04ea62af4c1b989d05327544c3ec73cd7eb
standarddeviant <dave.crist@gmail.com>
2020-07-10 01:22:00 -0400

    making suggested changes

--- src/utility/MPU6886.cpp
+++ src/utility/MPU6886.cpp
@@ -97,13 +97,7 @@ int MPU6886::Init(void){
   return 0;
 }
 
-#define WOM_STEP1_PWR_MGMT_1_CYCLE_SLEEP_GYRO_STANDBY_000(r) (r & 0x8F) // zero bits 6,5,4 of 7:0
-#define WOM_STEP1_PWR_MGMT_2_STBY_XYZ_A_000_G_111 0x07 // zero bits 5,4,3 and one bits 2,1,0 of 5:0
-#define WOM_STEP2_ACCEL_CONFIG2_FCHOICE_B_0_DLPF_CFG_001(r) (0x21) // average 32 samples, use 218 Hz DLPF
-#define WOM_STEP2P5_INT_PIN_CFG_ACTIVE_LOW_NO_LATCH(r) ((r | 0x88) & 0xDF)
-#define WOM_STEP3_INT_ENABLE_WOM_INT_EN_111 0xE0 // one bits 7,6,5, zero bits 4,3,2,1,0 of 7:0
-#define WOM_STEP5_ACCEL_INTEL_CTRL_INTEL_EN_1_MODE_1_WOM_TH_MODE_0 0xC2 // one bits 7,6,1 of 7:0
-#define WOM_STEP8_PWR_MGMT_1_CYCLE_1(r) (r | 0x20)
+
 void MPU6886::enableWakeOnMotion(Ascale ascale, uint8_t thresh_num_lsb) {
     uint8_t regdata;
     /* 5.1 WAKE-ON-MOTION INTERRUPT
@@ -125,28 +119,28 @@ void MPU6886::enableWakeOnMotion(Ascale ascale, uint8_t thresh_num_lsb) {
         • In PWR_MGMT_2 register (0x6C) set STBY_XA = STBY_YA = STBY_ZA = 0, and STBY_XG = STBY_YG = STBY_ZG = 1
     */
     I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
-    regdata = WOM_STEP1_PWR_MGMT_1_CYCLE_SLEEP_GYRO_STANDBY_000(regdata);
+    regdata = regdata & 0b10001111; // set cyle, sleep, and gyro to standby, i.e. 0
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
 
-    regdata = WOM_STEP1_PWR_MGMT_2_STBY_XYZ_A_000_G_111;
+    regdata = 0b00000111; // set accel x, y, and z to standby 
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_2, 1, &regdata);
 
     /* Step 2: Set Accelerometer LPF bandwidth to 218.1 Hz
         • In ACCEL_CONFIG2 register (0x1D) set ACCEL_FCHOICE_B = 0 and A_DLPF_CFG[2:0] = 1 (b001)
     */
     I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG2, 1, &regdata);
-    regdata = WOM_STEP2_ACCEL_CONFIG2_FCHOICE_B_0_DLPF_CFG_001(regdata);
+    regdata = 0b00100001; // average 32 samples, use 218 Hz DLPF
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_CONFIG2, 1, &regdata);
 
     /* Step 2.5 - active low? */
     I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_INT_PIN_CFG, 1, &regdata);
-    regdata = WOM_STEP2P5_INT_PIN_CFG_ACTIVE_LOW_NO_LATCH(regdata);
+    regdata =  ((regdata | 0b10000000) & 0b11011111); // configure pin active-low, no latch
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_PIN_CFG, 1, &regdata);
 
     /* Step 3: Enable Motion Interrupt
         • In INT_ENABLE register (0x38) set WOM_INT_EN = 111 to enable motion interrupt
     */
-    regdata = WOM_STEP3_INT_ENABLE_WOM_INT_EN_111;
+    regdata = 0b11100000; // enable wake-on-motion interrupt for X, Y, and Z axes
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_INT_ENABLE, 1, &regdata);
     
     /* Step 4: Set Motion Threshold
@@ -154,7 +148,7 @@ void MPU6886::enableWakeOnMotion(Ascale ascale, uint8_t thresh_num_lsb) {
         NOTE: the data sheet mentions 0x1F, but is probably referring to
               registers 0x20, 0x21, and 0x22 based on empirical tests
     */
-    regdata = thresh_num_lsb;
+    regdata = thresh_num_lsb; // set accel motion threshold for X, Y, and Z axes
     I2C_Write_NBytes(MPU6886_ADDRESS, 0x20, 1, &regdata);
     I2C_Write_NBytes(MPU6886_ADDRESS, 0x21, 1, &regdata);
     I2C_Write_NBytes(MPU6886_ADDRESS, 0x22, 1, &regdata);
@@ -163,7 +157,8 @@ void MPU6886::enableWakeOnMotion(Ascale ascale, uint8_t thresh_num_lsb) {
         • In ACCEL_INTEL_CTRL register (0x69) set ACCEL_INTEL_EN = ACCEL_INTEL_MODE = 1;
           Ensure that bit 0 is set to 0
     */
-    regdata = WOM_STEP5_ACCEL_INTEL_CTRL_INTEL_EN_1_MODE_1_WOM_TH_MODE_0;
+    regdata = 0b11000010; // enable wake-on-motion if any of X, Y, or Z axes is above threshold
+    // WOM_STEP5_ACCEL_INTEL_CTRL_INTEL_EN_1_MODE_1_WOM_TH_MODE_0;
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_ACCEL_INTEL_CTRL, 1, &regdata);
 
     /* Step 7: Set Frequency of Wake-Up
@@ -183,7 +178,7 @@ void MPU6886::enableWakeOnMotion(Ascale ascale, uint8_t thresh_num_lsb) {
         • In PWR_MGMT_1 register (0x6B) set CYCLE = 1
     */
     I2C_Read_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
-    regdata = WOM_STEP8_PWR_MGMT_1_CYCLE_1(regdata);
+    regdata = regdata | 0x00100000; // enable accelerometer low-power mode
     I2C_Write_NBytes(MPU6886_ADDRESS, MPU6886_PWR_MGMT_1, 1, &regdata);
 }
 
