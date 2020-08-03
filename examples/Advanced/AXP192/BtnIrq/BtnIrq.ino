0d6328881d3e91733293b3ea59cf98981877d6f6
sorz <heycoolbin@gmail.com>
2020-08-03 11:13:41 +0800

    Update AXP192 btn press irq example

--- examples/Advanced/AXP192/BtnIrq/BtnIrq.ino
+++ examples/Advanced/AXP192/BtnIrq/BtnIrq.ino
@@ -1,44 +1,13 @@
 #include "M5StickC.h"
 
 static void clear_mpu6880IRQ() {
-  uint8_t temp;
-  // set int active is LOW
-  M5.I2C.writeByte(0x68, 0x37, 0xc8);
-  // disable irq
-  M5.I2C.writeByte(0x68, 0x38, 0x00);
-  // clear irq status
-  M5.I2C.readByte(0x68, 0x39, &temp);
+  M5.Mpu6886.DisableAllIRQ();
+  M5.Mpu6886.SetINTPinActiveLogic(0);
+  M5.Mpu6886.ClearAllIRQ();
 }
 
 static void clear_200qIRQ() {
   M5.I2C.writeByte(0x6C, 0x13, 0xe0);
-}
-
-static void axp192_disableAllIRQ() {
-  M5.I2C.writeByte(0x34, 0x40, 0x00);
-  M5.I2C.writeByte(0x34, 0x41, 0x00);
-  M5.I2C.writeByte(0x34, 0x42, 0x00);
-  M5.I2C.writeByte(0x34, 0x43, 0x00);
-}
-
-static void axp192_clearAllIRQStatus() {
-  M5.I2C.writeByte(0x34, 0x44, 0xff);
-  M5.I2C.writeByte(0x34, 0x45, 0xff);
-  M5.I2C.writeByte(0x34, 0x46, 0xff);
-  M5.I2C.writeByte(0x34, 0x47, 0xff);
-  M5.I2C.writeByte(0x34, 0x4d, 0xff);
-}
-
-static void axp192_enableBtnIRQ() {
-  M5.I2C.writeByte(0x34, 0x42, 0x03);
-}
-
-static uint8_t axp192_getBtnIRQStatus() {
-  uint8_t status;
-  M5.I2C.readByte(0x34, 0x46, &status);
-  // clear btn irq
-  M5.I2C.writeByte(0x34, 0x46, 0xff);
-  return status;
 }
 
 void setup() {
@@ -48,19 +17,26 @@ void setup() {
   Wire.begin(21, 22, 400000);
   clear_mpu6880IRQ();
   clear_200qIRQ();
+  M5.Axp.DisableAllIRQ();
+  M5.Axp.ClearAllIRQ();
+  M5.Axp.EnablePressIRQ(true, true);
   pinMode(35, INPUT);
-  axp192_disableAllIRQ();
-  axp192_clearAllIRQStatus();
-  axp192_enableBtnIRQ();
 }
 
 void loop() {
   delay(1000);
   M5.Lcd.setCursor(0, 0);
   uint8_t PinStatus = digitalRead(35);
-  uint8_t IRQStatus = axp192_getBtnIRQStatus();
+  bool short_press, long_press;
+  M5.Axp.GetPressIRQ(&short_press, &long_press);
+  
+  if (short_press || long_press) {
+    M5.Axp.ClearPressIRQ(short_press, long_press);
+  }
+
   M5.Lcd.printf("pin 35 status: %d\r\n", PinStatus);
-  M5.Lcd.printf("axp192 btn status: %d\r\n", IRQStatus);
+  M5.Lcd.printf("axp192 btn\r\n  short press: %d\r\n  long press: %d\r\n", short_press, long_press);
+
   Serial.printf("pin 35 status: %d\r\n", PinStatus);
-  Serial.printf("axp192 btn status: %d\r\n", IRQStatus);
+  Serial.printf("axp192 btn\r\n  short press: %d\r\n  long press: %d\r\n", short_press, long_press);
 }
