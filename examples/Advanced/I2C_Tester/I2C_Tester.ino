9e7c6d04e2524a248f1ff7df11cb904977e96139
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-05 17:52:58 +0800

    change

--- examples/Advanced/I2C_Tester/I2C_Tester.ino
+++ examples/Advanced/I2C_Tester/I2C_Tester.ino
@@ -23,7 +23,7 @@ void setup()
   //Wire.begin(21, 22); //Detect internal I2C, if this sentence is not added, it will detect external I2C.  检测内部I2C,若不加此句为检测外部I2C
   M5.Lcd.setTextColor(YELLOW);  //Set the font color to yellow.  设置字体颜色为黄色
   M5.Lcd.setRotation(3);  //Rotate the screen.  旋转屏幕
-  M5.Lcd.println("M5Core2 I2C Tester"); //Print a string on the screen.  在屏幕上打印字符串
+  M5.Lcd.println("M5StickC I2C Tester"); //Print a string on the screen.  在屏幕上打印字符串
 
   delay(3000);
   M5.Lcd.fillScreen( BLACK );//Make the screen full of black (equivalent to clear() to clear the screen).  使屏幕充满黑色(等效clear()清屏)
