387c9846daf6330e46669d372735ac9e898338c8
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-16 15:31:34 +0800

    fan

--- examples/Unit/OP90.180_ITR9606/OP90.180_ITR9606.ino
+++ examples/Unit/OP90.180_ITR9606/OP90.180_ITR9606.ino
@@ -1,28 +1,29 @@
 /*
-    Description: Detect the current OP 90/180 Unit Photoelectric switch status.
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Stick-C sample source code
+*                          配套  M5Stick-C 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/m5stickc
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/m5stickc
+*
+* describe：OP 180/90.  非接触式光电限位开关
+* date：2021/8/16
+*******************************************************************************
+  Please connect to Port B(33),Detect the current OP 90/180 Unit Photoelectric switch status.
+  请连接端口B(33),检测当前OP 90/180单元光电开关状态。
 */
 
 #include <M5StickC.h>
 
-
 void setup() {
-  // put your setup code here, to run once:
-  M5.begin();
-  //Wire.begin();
-  M5.Lcd.setCursor(20, 5, 2);
-  M5.Lcd.println("90/180");
-    M5.Lcd.setCursor(20, 20, 2);
-  M5.Lcd.println("OPTICAL");
-
-  pinMode(33,INPUT_PULLUP);
-
+  M5.begin(); //Init M5StickC.  初始化M5StickC
+  M5.Lcd.setRotation(3);  //Rotate the screen.  旋转屏幕
+  M5.Lcd.setCursor(30, 10); //Set the cursor at (80,10).  将光标设置在(80,10)处
+  M5.Lcd.println("90/180 OPTICAL");
+  pinMode(33,INPUT_PULLUP); //Set pin 33 to input pull-up mode.  设置33号引脚为输入上拉模式
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  M5.Lcd.setCursor(8, 50, 2);
-  M5.Lcd.printf("IR receive:");
-   M5.Lcd.setCursor(15, 80 ,8);
-  M5.Lcd.printf("%d",digitalRead(33));
-
+  M5.Lcd.setCursor(30, 40);
+  M5.Lcd.printf("IR Receive: %d",digitalRead(33));  //Output the value of pin 33.  输出33号引脚的值
 }
--- examples/Unit/Relay/Relay.ino
+++ examples/Unit/Relay/Relay.ino
@@ -1,17 +1,37 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Stick-C sample source code
+*                          配套  M5Stick-C 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/m5stickc
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/m5stickc
+*
+* describe：Relay.  继电器
+* date：2021/8/16
+*******************************************************************************
+  Please connect to Port (32),Use RELAY to switch on and off the circuit.
+  请连接端口(32),使用继电器开关电路。
+*/
+
 #include <M5StickC.h>
 
 void setup() {
-  M5.begin();
-  //M5.Lcd.clear(BLACK);
-  M5.Lcd.setTextFont(4);
-  M5.Lcd.setTextColor(YELLOW, BLACK);
-  //disable the speak noise
+  M5.begin(); //Init M5StickC.  初始化 M5StickC
+  M5.Lcd.setRotation(3);  //Rotate the screen.  旋转屏幕
+  M5.Lcd.setCursor(50, 0);
+  M5.Lcd.println(("Relay Example"));
+  dacWrite(25, 0);  //disable the speak noise
   pinMode(32, OUTPUT);
 }
 
 void loop(void) {
+  M5.Lcd.setCursor(30, 40);
+  M5.Lcd.print("ON");
   digitalWrite(32, HIGH);
-  delay(500);
+  delay(1000);
+  M5.Lcd.fillRect(30,40,60,50,BLACK);
+  M5.Lcd.print("OFF");
   digitalWrite(32, LOW);
-  delay(500);
+  delay(1000);
+  M5.Lcd.fillRect(30,40,60,50,BLACK);
 }
