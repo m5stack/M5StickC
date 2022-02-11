99727137b90be2442ee907020d853416fe8adc38
Sean Kwok <45260108+Gitshaoxiang@users.noreply.github.com>
2022-02-11 19:33:10 +0800

    update power c examples

--- examples/Hat/PowerC_IP5209/PowerC_IP5209.ino
+++ examples/Hat/PowerC_IP5209/PowerC_IP5209.ino
@@ -1,25 +1,26 @@
 #include <M5StickC.h>
 #include <Wire.h>
-#include "PowerC.h"
+#include "M5_IP5209.h"
+TFT_eSprite canvas = TFT_eSprite(&M5.Lcd);
 
-POWERC PowerC;
+M5_IP5209 powerc;
 
 void setup() {
   M5.begin();
-  PowerC.Init();
+  M5.Lcd.setRotation(1);
+  canvas.createSprite(160, 80);
+  canvas.setTextColor(ORANGE);
+  powerc.begin();
 }
 
+char info[50];
+
 void loop() {
-  float v = PowerC.ReadVoltage();
-  float i = PowerC.ReadCurrent();
-  v = v * 0.00026855 + 2.6;
-  i = i * 0.745985;
-  M5.Lcd.setCursor(0,10);
-  M5.Lcd.printf("%1.2f V",v);
-  Serial.printf("%1.2f",v);
-  Serial.println("");
-  M5.Lcd.setCursor(0,40);
-  M5.Lcd.printf("%1.0f mAh",i);
-  Serial.printf("%1.0f",i);
-  Serial.println("");
+  canvas.fillSprite(BLACK);
+  canvas.setCursor(0,10);
+  float v = powerc.getBatVoltage();
+  float i = powerc.getBatCurrent();
+  sprintf(info, "%1.2fV   %1.0fmAh",v,  i);
+  canvas.println(info);
+  canvas.pushSprite(10, 0);
 }
