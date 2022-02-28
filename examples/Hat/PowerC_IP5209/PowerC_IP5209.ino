1905cdec650f82d73a4cd04de274375d0e4f3b12
Sean Kwok <45260108+Gitshaoxiang@users.noreply.github.com>
2022-02-28 16:44:50 +0800

    update PowerC example

--- examples/Hat/PowerC_IP5209/PowerC_IP5209.ino
+++ examples/Hat/PowerC_IP5209/PowerC_IP5209.ino
@@ -6,21 +6,39 @@ TFT_eSprite canvas = TFT_eSprite(&M5.Lcd);
 M5_IP5209 powerc;
 
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(1);
-  canvas.createSprite(160, 80);
-  canvas.setTextColor(ORANGE);
-  powerc.begin();
+    M5.begin();
+    M5.Lcd.setRotation(1);
+    canvas.createSprite(160, 80);
+    canvas.setTextColor(ORANGE);
+    while (!powerc.begin()) {
+        canvas.fillSprite(BLACK);
+        canvas.setCursor(0, 10);
+        canvas.println("POWERC INIT ERROR");
+        canvas.println("Pls click the power btn and check the I2C connect.");
+        canvas.pushSprite(10, 0);
+        delay(1000);
+    }
 }
 
 char info[50];
 
 void loop() {
-  canvas.fillSprite(BLACK);
-  canvas.setCursor(0,10);
-  float v = powerc.getBatVoltage();
-  float i = powerc.getBatCurrent();
-  sprintf(info, "%1.2fV   %1.0fmAh",v,  i);
-  canvas.println(info);
-  canvas.pushSprite(10, 0);
+    if (powerc.update()) {
+        float v     = powerc.voltage;
+        float i     = powerc.current;
+        float p     = powerc.power;
+        bool charge = powerc.charged;
+        canvas.fillSprite(BLACK);
+        canvas.setCursor(0, 10);
+        sprintf(info, "BAT: %1.2fV\r\n%1.2fmA\r\npower:%1.2fw\r\nischarge: %d",
+                v, i, p, charge);
+        canvas.println(info);
+        canvas.pushSprite(10, 0);
+        delay(100);
+    } else {
+        canvas.fillSprite(BLACK);
+        canvas.setCursor(0, 10);
+        canvas.println("powerc update error..");
+        canvas.pushSprite(10, 0);
+    }
 }
